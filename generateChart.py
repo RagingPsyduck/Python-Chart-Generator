fa8a6180046c20c69b902f258d08286b1fe81559
teng <yangteng0813@gmail.com>
2017-11-30 20:29:34 -0500

    11.30

--- generateChart.py
+++ generateChart.py
@@ -1,7 +1,17 @@
 
 import matplotlib.pyplot as plt
 
-plt.plot([50,100,150,200,250,300], [639,875,1179,1279,1663,1853], 'ro')
+line1 = plt.plot([100,200,500,1000,1500,2000], [2011,2820,6578,14369,21551,31948],label='One Process')
+line2 = plt.plot([100,200,500,1000,1500,2000], [204,211,350,585,443,686],label='Two processes - Process 1')
+line3 = plt.plot([100,200,500,1000,1500,2000], [1098,1281,4753,10526,7694,17863],label='Two processes - Process 2')
+
+
+plt.setp(line1, color='r', linewidth=2.0)
+plt.setp(line2, color='b', linewidth=2.0)
+plt.setp(line3, color='g', linewidth=2.0)
+
+
+plt.legend(loc='upper left')
 plt.ylabel('Times')
 plt.xlabel('Number of nodes')
 plt.show()
